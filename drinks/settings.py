6e42bbf306c3713c8a9c61487cff3aac808c0ed7
Irvin Campos <irvincampos76@gmail.com>
2021-08-20 03:57:45 -0700

    ...

--- drinks/settings.py
+++ drinks/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from . import secrets
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ss_wb@s%w@nv_9yv0ke*lla^s^^p6w2u!v)qdnq-#4%$ombxk$'
+SECRET_KEY =  secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
